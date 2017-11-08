41ad6f8e522046fa346b9f086b4d51eafc3881cb
Melissa Wolowicz <melissawolowicz@MW-iMac.local>
2017-11-08 13:24:03 -0500

    Fixed all isues with wrong user choices and low possiblitites of wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,15 @@
-/* User selects a letter
-Get the computer to make a random letter choice
+/* BASICS FOR THE GAME:
+Computer makes a random letter choice
+User selects a letter
+Make sure user selection is a letter
 Capture the computer's choice and the user's guess
 Compare the choice and the guess
 Determine Wins & Losses
 Count the attempts made by user
 Record all guesses made by user
+When user wins, reset game, but not the stats
+When user runs out of guesses, reset game, but not the stats
 Display all game stats to the user */
-
 
 // Create an Array of options for both computer and user to choose from
 var theAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -16,49 +19,63 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var soFar = [];
+var compChoice; //declared only
 
-//Script starts when user presses a key, on the key release (onkeyup):
+// Initially creating a new computer choice
+	compChoice = theAlphabet[Math.floor(Math.random() * theAlphabet.length)];
+		console.log("The computer has chosen the letter: " + compChoice);
+
+//Script that starts every time the user presses a key, on the key release (onkeyup):
 document.onkeyup = function(event) {
-
-	// The computer picks a random letter from theAlphabet, record this letter under var compChoice.
-	var compChoice = theAlphabet[Math.floor(Math.random() * theAlphabet.length)];
-		console.log(compChoice);
 
 	//User presses a key, record what key was pressed under var userGuess and convert to lower case.
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log(userGuess);
 
-	//Log all keys pressed by user into soFar by pushing them into the empty array, display to user.
-	soFar.push(userGuess);
-	document.getElementById("soFar").innerHTML = soFar.join(", ");
-
-	//Compare the userGuess and the compChoice to determine wins and losses
-		//If the computer's choice and the user's guess is the same, increase the wins count, and decrease number of guessesLeft.
-		if (compChoice === userGuess) {
-			wins++;
-			guessesLeft--;
-			//Display updated stats to user:
-			document.getElementById("wins").innerHTML = wins;
-			document.getElementById("guessesLeft").innerHTML = guessesLeft;
-			alert("You ARE a psychic! How did you know I was thinking of the letter '" + userGuess + "' ?");
+	//Log all keys pressed by user into soFar by pushing them into the empty array, DO NOT LOG WRONG KEYS display to user.
+	if (event.keyCode >= 65 && event.keyCode <= 90) {
+		soFar.push(userGuess);
+		document.getElementById("soFar").innerHTML = soFar.join(", ");
+	}
 
 
-		} else { //If it's not the same, increase the losses count, and decrease number of guessesLeft.
-			losses++;
-			guessesLeft--;
-			//Display updated stats to user:
-			document.getElementById("losses").innerHTML = losses;
-			document.getElementById("guessesLeft").innerHTML = guessesLeft;
-			alert("Sorry! '" + userGuess + "' is NOT the letter I am thinking of right now. Try again, you have " + guessesLeft + " guesses left.")
+	//Check if the user entered the correct letters
+		if (event.keyCode >= 65 && event.keyCode <= 90) { //If the letter pressed IS part of the alphabet, then run the comparison:
+			//If user guesses right,increase the wins count, and decrease number of guessesLeft:
+			if (userGuess == compChoice) {
+				wins++;
+				guessesLeft--;
+				//Display updated stats to user:
+				document.getElementById("wins").innerHTML = wins;
+				document.getElementById("guessesLeft").innerHTML = guessesLeft;
+				alert("You ARE a psychic! How did you know I was thinking of the letter '" + userGuess + "' ?");
+				//now Choose another letter
+				compChoice = theAlphabet[Math.floor(Math.random() * theAlphabet.length)];
+				console.log("The computer has chosen the letter: " + compChoice);	
+
+			} else { //If user guesses wrong, increase the losses count, and decrease number of guessesLeft.
+				losses++;
+				guessesLeft--;
+				//Display updated stats to user:
+				document.getElementById("losses").innerHTML = losses;
+				document.getElementById("guessesLeft").innerHTML = guessesLeft;
+			}
+		} else { //If user input is not a letter from the alphabet
+			alert("Please be sure to select a letter from the Alphabet (from a to z)");
 		}
 
 	//When the user has no more guesses left, reset the counters back to zero.
-		//If the guessesLeft == 0, reset guessesLeft back to 10, and empty the soFar guesses.
+		//If the guessesLeft == 0, reset guessesLeft back to 10, and empty the soFar guesses, pick a new letter.
 		if (guessesLeft == 0) {
 			guessesLeft = 10;
 			document.getElementById("guessesLeft").innerHTML = guessesLeft;
 			soFar = [];
 			document.getElementById("soFar").innerHTML = soFar.join(", ");
-			alert("OH NO! You have run out of guesses. Don't worry, keep trying!")
+			alert("GAME OVER")
+			alert("OH NO! You have run out of guesses. Do you want to try again?")
+			//now Choose another letter
+			compChoice = theAlphabet[Math.floor(Math.random() * theAlphabet.length)];
+			console.log("The computer has chosen the letter: " + compChoice);
 		}	
 }
+
